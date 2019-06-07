b899d80c8a641ef636a99e55a52bf508292c5e84
StrangerZhang <fangyizhangz@gmail.com>
2019-06-07 20:34:33 +0800

    add local_rank to train.py

--- tools/train.py
+++ tools/train.py
@@ -39,6 +39,8 @@ parser.add_argument('--cfg', type=str, default='config.yaml',
                     help='configuration of tracking')
 parser.add_argument('--seed', type=int, default=123456,
                     help='random seed')
+parser.add_argument('--local_rank', type=int, default=0,
+                    help='compulsory for pytorch launcer')
 args = parser.parse_args()
 
 
