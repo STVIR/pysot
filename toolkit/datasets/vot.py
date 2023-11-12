355a63c983d2b9f6bf98ee910448337a8da482b6
CoderMungan <codermungan@gmail.com>
2023-11-12 06:24:46 +0300

    Fixed Issue STVIR#294

--- toolkit/datasets/vot.py
+++ toolkit/datasets/vot.py
@@ -27,9 +27,9 @@ class VOTVideo(Video):
     """
     def __init__(self, name, root, video_dir, init_rect, img_names, gt_rect,
             camera_motion, illum_change, motion_change, size_change, occlusion, load_img=False):
-        super(VOTVideo, self).__init__(name, root, video_dir,
-                init_rect, img_names, gt_rect, None, load_img)
-        self.tags= {'all': [1] * len(gt_rect)}
+        img_names = [os.path.join(root, video_dir, img_name) for img_name in img_names]
+        super(VOTVideo, self).__init__(name, root, video_dir, init_rect, img_names, gt_rect, None, load_img)
+        self.tags = {'all': [1] * len(gt_rect)}
         self.tags['camera_motion'] = camera_motion
         self.tags['illum_change'] = illum_change
         self.tags['motion_change'] = motion_change
@@ -43,10 +43,8 @@ class VOTVideo(Video):
         #                         for x in self.gt_traj]
 
         # empty tag
-        all_tag = [v for k, v in self.tags.items() if len(v) > 0 ]
+        all_tag = [v for k, v in self.tags.items() if len(v) > 0]
         self.tags['empty'] = np.all(1 - np.array(all_tag), axis=1).astype(np.int32).tolist()
-        # self.tags['empty'] = np.all(1 - np.array(list(self.tags.values())),
-        #         axis=1).astype(np.int32).tolist()
 
         self.tag_names = list(self.tags.keys())
         if not load_img:
@@ -61,14 +59,8 @@ class VOTVideo(Video):
         return self.tags[tag][start:end]
 
     def load_tracker(self, path, tracker_names=None, store=True):
-        """
-        Args:
-            path(str): path to result
-            tracker_name(list): name of tracker
-        """
         if not tracker_names:
-            tracker_names = [x.split('/')[-1] for x in glob(path)
-                    if os.path.isdir(x)]
+            tracker_names = [x.split('/')[-1] for x in glob(path) if os.path.isdir(x)]
         if isinstance(tracker_names, str):
             tracker_names = [tracker_names]
         for name in tracker_names:
@@ -80,8 +72,7 @@ class VOTVideo(Video):
             pred_traj = []
             for traj_file in traj_files:
                 with open(traj_file, 'r') as f:
-                    traj = [list(map(float, x.strip().split(',')))
-                            for x in f.readlines()]
+                    traj = [list(map(float, x.strip().split(','))) for x in f.readlines()]
                     pred_traj.append(traj)
             if store:
                 self.pred_trajs[name] = pred_traj
@@ -100,7 +91,6 @@ class VOTDataset(Dataset):
         with open(os.path.join(dataset_root, name+'.json'), 'r') as f:
             meta_data = json.load(f)
 
-        # load videos
         pbar = tqdm(meta_data.keys(), desc='loading '+name, ncols=100)
         self.videos = {}
         for video in pbar:
@@ -118,9 +108,7 @@ class VOTDataset(Dataset):
                                           meta_data[video]['occlusion'],
                                           load_img=load_img)
 
-        self.tags = ['all', 'camera_motion', 'illum_change', 'motion_change',
-                     'size_change', 'occlusion', 'empty']
-
+        self.tags = ['all', 'camera_motion', 'illum_change', 'motion_change', 'size_change', 'occlusion', 'empty']
 
 class VOTLTVideo(Video):
     """
@@ -132,40 +120,29 @@ class VOTLTVideo(Video):
         img_names: image names
         gt_rect: groundtruth rectangle
     """
-    def __init__(self, name, root, video_dir, init_rect, img_names,
-            gt_rect, load_img=False):
-        super(VOTLTVideo, self).__init__(name, root, video_dir,
-                init_rect, img_names, gt_rect, None, load_img)
-        self.gt_traj = [[0] if np.isnan(bbox[0]) else bbox
-                for bbox in self.gt_traj]
+    def __init(self, name, root, video_dir, init_rect, img_names, gt_rect, load_img=False):
+        img_names = [os.path.join(root, video_dir, img_name) for img_name in img_names]
+        super(VOTLTVideo, self).__init__(name, root, video_dir, init_rect, img_names, gt_rect, None, load_img)
+        self.gt_traj = [[0] if np.isnan(bbox[0]) else bbox for bbox in self.gt_traj]
         if not load_img:
-            img_name = os.path.join(root, self.img_names[0])
+            img_name = os.path.join(root, img_names[0])
             img = np.array(Image.open(img_name), np.uint8)
             self.width = img.shape[1]
             self.height = img.shape[0]
         self.confidence = {}
 
     def load_tracker(self, path, tracker_names=None, store=True):
-        """
-        Args:
-            path(str): path to result
-            tracker_name(list): name of tracker
-        """
         if not tracker_names:
-            tracker_names = [x.split('/')[-1] for x in glob(path)
-                    if os.path.isdir(x)]
+            tracker_names = [x.split('/')[-1] for x in glob(path) if os.path.isdir(x)]
         if isinstance(tracker_names, str):
             tracker_names = [tracker_names]
         for name in tracker_names:
-            traj_file = os.path.join(path, name, 'longterm',
-                    self.name, self.name+'_001.txt')
+            traj_file = os.path.join(path, name, 'longterm', self.name, self.name+'_001.txt')
             with open(traj_file, 'r') as f:
-                traj = [list(map(float, x.strip().split(',')))
-                        for x in f.readlines()]
+                traj = [list(map(float, x.strip().split(','))) for x in f.readlines()]
             if store:
                 self.pred_trajs[name] = traj
-            confidence_file = os.path.join(path, name, 'longterm',
-                    self.name, self.name+'_001_confidence.value')
+            confidence_file = os.path.join(path, name, 'longterm', self.name, self.name+'_001_confidence.value')
             with open(confidence_file, 'r') as f:
                 score = [float(x.strip()) for x in f.readlines()[1:]]
                 score.insert(0, float('nan'))
@@ -185,15 +162,8 @@ class VOTLTDataset(Dataset):
         with open(os.path.join(dataset_root, name+'.json'), 'r') as f:
             meta_data = json.load(f)
 
-        # load videos
         pbar = tqdm(meta_data.keys(), desc='loading '+name, ncols=100)
         self.videos = {}
         for video in pbar:
             pbar.set_postfix_str(video)
-            self.videos[video] = VOTLTVideo(video,
-                                          dataset_root,
-                                          meta_data[video]['video_dir'],
-                                          meta_data[video]['init_rect'],
-                                          meta_data[video]['img_names'],
-                                          meta_data[video]['gt_rect'])
-
+            self.videos[video] = VOTLTVideo(video, dataset_root, meta_data[video]['video_dir'], meta_data[video]['init_rect'], meta_data[video]['img_names'], meta_data[video]['gt_rect'])
